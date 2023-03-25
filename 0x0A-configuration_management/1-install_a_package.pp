f57a9ef0ae52f77308008a13e9092e295b30a2dc
musaab moha <111170428+musaabmoha@users.noreply.github.com>
2023-03-25 15:57:31 +0300

    Update 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,32 @@
+package { 'python3-pip':
+  ensure => installed,
+}
+
+package { 'python3-dev':
+  ensure => installed,
+}
+
+package { 'build-essential':
+  ensure => installed,
+}
+
+package { 'libssl-dev':
+  ensure => installed,
+}
+
+package { 'libffi-dev':
+  ensure => installed,
+}
+
 package { 'flask':
-ensure => installed,
-source => python3-pip,
-version => 2.1.0,
+  ensure   => '2.1.0',
+  provider => 'pip3',
+  require  => [
+    Package['python3-pip'],
+    Package['python3-dev'],
+    Package['build-essential'],
+    Package['libssl-dev'],
+    Package['libffi-dev'],
+  ],
 }
 
